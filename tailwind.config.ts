03b3db8b81c4bf30a336f6627b85934d33325f8b
imLeonam <leonammrib@gmail.com>
2024-09-03 02:33:10 -0300

    feat(tailwind): added animations in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,41 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        slideInLeft: {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        slideOutLeft: {
+          "0%": { transform: "translateX(0)", opacity: "1" },
+          "100%": { transform: "translateX(-100%)", opacity: "0" },
+        },
+        slideInFromRight: {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        slideOutToRight: {
+          "0%": { transform: "translateX(0)", opacity: "1" },
+          "100%": { transform: "translateX(100%)", opacity: "0" },
+        },
+        shake: {
+          "0%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(-5px)" },
+          "50%": { transform: "translateX(5px)" },
+          "75%": { transform: "translateX(-5px)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        slideInLeft: "slideInLeft 0.5s cubic-bezier(0.22, 0.61, 0.36, 1) both",
+        slideOutLeft:
+          "slideOutLeft 0.5s cubic-bezier(0.22, 0.61, 0.36, 1) both",
+        slideInRight:
+          "slideInFromRight 0.5s cubic-bezier(0.22, 0.61, 0.36, 1) both",
+        slideOutRight:
+          "slideOutToRight 0.5s cubic-bezier(0.22, 0.61, 0.36, 1) both",
+        shake: "shake 0.3s cubic-bezier(0.22, 0.61, 0.36, 1) both",
+      },
     },
   },
   plugins: [],
