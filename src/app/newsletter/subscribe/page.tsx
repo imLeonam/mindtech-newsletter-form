587dc52d0d8cdbbe0a2d9bd46bd48ff9bfecd0d3
imLeonam <leonammrib@gmail.com>
2024-09-03 14:43:40 -0300

    feat(newsletter): added responsivity in subscribe page

--- src/app/newsletter/subscribe/page.tsx
+++ src/app/newsletter/subscribe/page.tsx
@@ -1,5 +1,4 @@
 import style from "@/app/ui/newsletter/styles/subscribe.module.css";
-import Image from "next/image";
 
 import InfoCard from "@/app/ui/newsletter/info-card";
 import Form from "@/app/ui/newsletter/subscribe-form";
@@ -8,13 +7,20 @@ import Ilustration from "@/app/ui/newsletter/ilustration";
 function Page() {
   return (
     <div className={style.container}>
-      <div className={`${style.card} animate-slideInLeft`}>
+      <div
+        className={`flex flex-nowrap flex-row sm:justify-center lg:justify-between items-center rounded-3xl sm:h-3/4 lg:h-4/5 lg:w-3/4
+           sm:w-1/2 md:px-24 sm:ml-5 sm:mr-5 md:py-28 bg-slate-950 animate-slideInLeft`}
+        style={{ marginLeft: 20, marginRight: 20 }}
+      >
         <div className={style.blueBlur} />
-        <div className={style.formContainer}>
+        <div
+          className="flex sm:justify-center lg:justify-start flex-col sm:w-full
+           lg:w-2/5 py-10 px-10 z-10"
+        >
           <InfoCard />
           <Form />
         </div>
-        <div className={style.imageContainer}>
+        <div className="flex justify-end self-center z-10">
           <Ilustration />
         </div>
       </div>
